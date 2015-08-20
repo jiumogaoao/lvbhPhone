480067da88583613e634f4eff98306f7a5863411
jiumogaoao <394127821@qq.com>
2015-08-20 16:57:27 +0800

    change
    change

--- control/travellerAdd.js
+++ control/travellerAdd.js
@@ -8,13 +8,15 @@
 			var head=_.template(data.tem[0])({left:"",center:"添加常用旅客",right:"保存"});
 			$("#head").html(head);
 
-			$("#scroller").html("");
+			$("#scroller").html(data.tem[1]);
 
 			$(".top_third .leftButton").unbind("tap").bind("tap",function(){
 				window.history.go(-1);
 				});
-				
-			myScroll.refresh();
+			var delay=setTimeout(function(){
+				myScroll.refresh();
+				},200);	
+			
 			
 			}
 		});
