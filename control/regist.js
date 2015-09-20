8d73044b514e1c1d72da8f698cce4d290b835464
jiumogaoao <394127821@qq.com>
2015-09-21 07:26:15 +0800

    change

--- control/regist.js
+++ control/regist.js
@@ -14,7 +14,36 @@
 				{"name":"key2","placehold":"请再次输入密码","icon":"fa-lock","value":"","other":""}
 				]});
 			var button=_.template(data.tem[3])({"text":"注册","id":"registButton"});
-			$("#scroller").html(data.tem[1]+list+button);
+			
+			function layout(at){
+				$("#scroller").html(data.tem[1]+list+button);
+			function delay(){
+				$("[name='code'] .other span").html("发送动态密码");
+				$("[name='code'] .other").unbind("tap").bind("tap",function(){
+				if($("[name='user'] input").val() && $("[name='user'] input").val().length){
+						obj.api.run("login_phone_message",{
+							at:at,
+							mobile:$("[name='user'] input").val()
+							},function(data){
+							$("[name='key'] .other").unbind("tap");
+							var total=60;
+							var timeOut=setInterval(function(){
+								if(total !== 0){
+									$("[name='key'] .other span").html(total+"秒后可再次发送");
+									total--;
+									}else{
+										clearInterval(timeOut);
+										delay();
+										}
+								},1000);
+						},function(e){
+						obj.pop.on("alert",{text:JSON.stringify(e)});
+						});	
+					}else{obj.pop.on("alert",{text:"请填写手机号"});}
+				});
+				}	
+			delay();
+			
 			$(".top_third .leftButton").unbind("tap").bind("tap",function(){
 				window.history.go(-1);
 				});
@@ -28,6 +57,8 @@
 			$('img').load(function(){
 				myScroll.refresh();
 				});
+				};
+			obj.api.at(layout);
 			}
 		});
 	})($,app,config);
